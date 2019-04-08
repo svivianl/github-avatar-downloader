68f551a3280c6ff8d3d41db0dbb9ce91a26a3c1d
vshibukawa <viviane.shibukawa@gmail.com>
2019-04-08 18:08:17 +0000

    add step 2

--- download_avatars.js
+++ download_avatars.js
@@ -1,18 +1,28 @@
+var app = require('request');
 var fs = require('fs');
 var dir = './avatars/';
 
-var myArgs = process.argv.slice(2);
-var repoOwner = myArgs[0];
-var repo = myArgs[1];
+// var myArgs = process.argv.slice(2);
+// var repoOwner = myArgs[0];
+// var repo = myArgs[1];
 
-// creates the directory
-if (!fs.existsSync(dir)){
-  // fs.mkdirSync(dir);
-  fs.mkdir(dir, { recursive: true }, (err) => {
-  if (err){
-    console.log(e.message);
-  }else{
-    //
-  }
-});
-}
+console.log('Welcome to the GitHub Avatar Downloader!');
+
+
+// // creates the directory
+// if (!fs.existsSync(dir)){
+//   // fs.mkdirSync(dir);
+//   fs.mkdir(dir, { recursive: true }, (err) => {
+//   if (err){
+//     console.log(e.message);
+//   }else{
+//     //
+//   }
+// });
+// }
+
+// app.
+
+// function getRepoContributors(){
+
+// }
